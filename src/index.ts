f3629df688d5241d83b0d6f60e479738a74a0c7b
barem21 <barem210@gmail.com>
2025-01-31 11:39:16 +0900

    [docs] 타입스크립트 인터페이스

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,15 @@
-//안타깝게도 팀장님이 작성한 타입이라서
-//우리가 고쳐서 활용하기는 어렵다.
-//원본을 수정할 수 없는데 우리는 타입을 구분해야 하는 경우
-type Dog = {
-  name: string; //이름
-  isBark: boolean; //짖는다.
+//아래 상황은 Person이 하나로 합쳐진다.
+interface Person {
+  name: string;
+}
+interface Person {
+  name: string;
+  age: number;
+}
+interface Male extends Person {
+  name: "gildong";
+}
+const who: Male = {
+  name: "고길동", //오류 발생
+  age: 10,
 };
-type Cat = {
-  name: string; //이름
-  isScratch: boolean; //할퀸다.
-};
-
-//정의되어진 타입을 활용한다. (타입을 사용할려는 개발자)
-type Animal = Dog | Cat;
-
-function go(ani: Animal) {
-  //타입가드
-  if ("isBark" in ani) {
-    console.log(ani + "는 강아지구만");
-  } else if ("isScratch" in ani) {
-    console.log(ani + "는 고양이구만");
-  }
-}
-
-//우리가 타입가드를 적용한 함수생성
-function isDog(ani: Animal): ani is Dog {
-  return (ani as Dog).isBark !== undefined;
-}
-function isCat(ani: Animal): ani is Cat {
-  return (ani as Cat).isScratch !== undefined;
-}
-
-function goType(ani: Animal) {
-  if (isDog(ani)) {
-    console.log(ani + "는 강아지구만");
-  } else if (isCat(ani)) {
-    console.log(ani + "는 고양이구만");
-  }
-}
