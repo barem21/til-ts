bbcedf26b5e7f2be82a5746984d912c50aa3aace
barem21 <barem210@gmail.com>
2025-02-12 09:54:36 +0900

    [docs] generic 기초

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
-//약속을 지켜라
-interface CarInterface {
-  name: string;
-  brand: string;
-  price: number;
-  move(): void;
-  stop(): void;
+//함수 리턴타입에서 리턴종류를 모르면
+//any, 또는 void을 리턴한다.
+//만약 원하는 것이 있다면 정확히 작성해야 한다.
+function showValue<T>(value: T): T {
+  return value;
 }
 
-interface ElectricInterface {
-  battery: number;
-  isBattery: boolean;
-}
-
-//인터페이스를 구현
-class ElectricCar implements CarInterface, ElectricInterface {
-  constructor(
-    public name: string,
-    public brand: string,
-    public price: number,
-    public battery: number,
-    public isBattery: boolean
-  ) {}
-  move() {
-    console.log("움직여라!");
-  }
-  stop() {
-    console.log("멈춰라!");
-  }
-}
-
-let modelX = new ElectricCar("modelX", "tesla", 10000, 10, true);
+let test = showValue(100);
+let test2 = showValue("hello");
+let test3 = showValue(true);
+let test4 = showValue([1, 2, 3]);
